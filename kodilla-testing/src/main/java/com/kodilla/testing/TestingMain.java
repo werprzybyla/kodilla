b11ffde88ffe1f221cc49f05415b373ad3e2ca0e
Weronika <weronika.gabriela.przybyla@gmail.com>
2023-01-25 15:21:33 +0100

    6.2 first tests

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,37 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        //SimpleUser test
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if(result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Calculator test
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(5,3);
+        int subtractResult = calculator.subtract(5, 3);
+
+        if(addResult == 8) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if(subtractResult == 2) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
