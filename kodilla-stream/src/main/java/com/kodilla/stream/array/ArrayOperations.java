2431db5d9964f965481fa317c4b6eec45284d74b
Weronika <weronika.gabriela.przybyla@gmail.com>
2023-03-17 11:33:28 +0100

    7.6 portfolio

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -7,7 +7,7 @@ public interface ArrayOperations {
     static double getAverage(int[] numbers) {
         IntStream.range(0,numbers.length)
                 .map(n -> numbers[n])
-                .forEach(n -> System.out.println(n));
+                .forEach(System.out::println);
 
          return IntStream.range(0,numbers.length)
                  .map(n -> numbers[n])
